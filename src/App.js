4bb7969490e0d7e94e774e8dd27659902a2bf7fc
Shannon FitzGerald <misfitz616@gmail.com>
2023-08-02 12:34:42 -0700

    add first website

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
+import lostcatsweb from "./image/lostcatsthumbnail.jpg";
 import "./App.css";
 import "bulma/css/bulma.min.css";
 
@@ -29,8 +30,32 @@ function App() {
           </div>
         </div>
       </nav>
-      <div className='bio has-text-centered'>
+      <div className='bio opensans has-text-centered'>
         <p>Full stack web and mobile developer based in Fresno, California. Experienced in developing custom responsive websites and apps using React, Python, Django, JavaScript, HTML, CSS and Bootstrap as well as Social Media content creation and management.</p>
+      </div>
+      <div>
+        <div className="heading anton has-text-centered is-size-2 is-uppercase pt-5">
+          Websites
+        </div>
+        <div className="row">
+          <div className="column is-one-third">
+            <a href="https://sfitzgerald16.github.io/Lost-Cats/" class="thumbnail" target="_blank">
+              {/* <div class="thumbnail-image-box" id="lost-cats-web" ></div> */ }
+              <img
+                src={ lostcatsweb }
+                alt="Lost Cats Website"
+                style={ { height: 200, marginBottom: 20, padding: "0 0 10px 0" } }
+                // className="logo"
+              />
+              <div class="caption text-center sans-text">
+                <h3>California Lost Cats</h3>
+              </div>
+              <div class="overlay"></div>
+            </a>
+          </div>
+          <div className="column is-one-third"></div>
+          <div className="column is-one-third"></div>
+        </div>
       </div>
     </div>
   );
